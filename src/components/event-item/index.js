676ba36bf0139763b1bf5b274f557eed83627bff
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-08-14 11:06:23 +0700

    Add moment for Event Item

--- src/components/event-item/index.js
+++ src/components/event-item/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { EnvironmentTwoTone, DollarTwoTone } from '@ant-design/icons';
+import moment from 'moment';
+import { EnvironmentTwoTone, ClockCircleTwoTone } from '@ant-design/icons';
 import './index.less';
-import { Typography, Space } from 'antd';
 
-const EventItem = ({ name, location, price, imageURL, description }) => {
+moment.defaultFormat = 'DD-MM-YYYY';
+
+const EventItem = ({ name, location, price, imageURL, description, time = '12-12-2020' }) => {
   return (
     <div className="event-wrapper">
       <div className="event-image-wrapper">
         <div className="event-image-info">
-          <div>
-            <p style={{ color: 'rgb(255, 215, 98)' }}>${price || 'N/A'}</p>
+          <div className="event-price">
+            <p>${price || 'N/A'}</p>
           </div>
         </div>
         <img src={imageURL} alt="Event Thumbnail" className="event-image" />
@@ -17,9 +19,11 @@ const EventItem = ({ name, location, price, imageURL, description }) => {
       <div className="event-content-wrapper">
         <div className="event-content">
           <h4 className="event-font">{name}</h4>
+          <p className="event-font" style={{ marginBottom: '0' }}>
+            <EnvironmentTwoTone twoToneColor="orange" /> {location}
+          </p>
           <p className="event-font">
-            <EnvironmentTwoTone twoToneColor="orange" />
-            {location}
+            <ClockCircleTwoTone /> {moment(time).format('dddd, DD-M-YYYY')}
           </p>
           {/* <Typography.Paragraph
               ellipsis={{ rows: 2, expandable: false, symbol: 'more' }}
